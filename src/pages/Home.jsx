b513579ae2c336673d469a7b5d5aefc5e5573cd9
niklasmikael <niklaspaajanen@hotmail.com>
2024-01-28 20:54:48 +0200

    vaihdettu banneri

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import helmet from '../images/trok-logo-helmet.png'
 import { Link } from "react-router-dom";
-import clouds from "../images/trok-clouds.png"
+import clouds from "../images/trok-banneri.png"
 
 
 function Home() {
